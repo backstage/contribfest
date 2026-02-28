2a471c2c52999db7dd07c3099d7da578b5ae6d53
Andre Wanlin <awanlin@spotify.com>
2026-02-27 19:07:42 -0600

    feat: add pinned issues data

---
+++ lib/pinned-issues.ts
@@ -0,0 +1,22 @@
+import type { PinnedIssue } from '@/lib/types'
+
+export const pinnedIssues: PinnedIssue[] = [
+  {
+    issueId: 7868,
+    repository: 'backstage/backstage',
+    level: 'Beginner',
+    title: 'TBD',
+  },
+  {
+    issueId: 2458,
+    repository: 'backstage/community-plugins',
+    level: 'Beginner',
+    title: 'TBD',
+  },
+  {
+    issueId: 7869,
+    repository: 'backstage/backstage',
+    level: 'Beginner',
+    title: 'TBD',
+  },
+]
